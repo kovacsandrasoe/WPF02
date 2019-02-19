d82cabbf730dcc7631a96e6a48d7840961ab14a2
Kovács András <kovi91@gmail.com>
2019-02-19 17:13:32 +0100

    #1: full

--- 01_Calc/MainWindow.xaml.cs
+++ 01_Calc/MainWindow.xaml.cs
@@ -20,9 +20,56 @@ namespace _01_Calc
     /// </summary>
     public partial class MainWindow : Window
     {
+        double memory;
+        double operand_b;
+        Dictionary<char, Func<double, double, double>> mathFunctions;
+        Func<double, double, double> selectedOperator;
         public MainWindow()
         {
             InitializeComponent();
+            memory = 0;
+            operand_b = 0;
+            mathFunctions = new Dictionary<char, Func<double, double, double>>();
+            mathFunctions.Add('+', (x, y) => x + y);
+            mathFunctions.Add('-', (x, y) => x - y);
+            mathFunctions.Add('*', (x, y) => x * y);
+            mathFunctions.Add('/', (x, y) => x / y);
+        }
+
+        private void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var btn = e.Source as Label;
+            if (btn != null && char.IsDigit(btn.Content.ToString()[0]))
+            {
+                //number
+                memory *= 10;
+                memory += double.Parse(btn.Content.ToString());
+                lb_display.Content = memory;
+            }
+            else if (btn != null && mathFunctions.Keys.Contains(btn.Content.ToString()[0]))
+            {
+                //operator in mathfunctions delegate
+                selectedOperator = mathFunctions[btn.Content.ToString()[0]];
+                operand_b = memory;
+                memory = 0;
+                lb_display.Content = memory;
+            }
+            else if (btn != null && btn.Content.ToString()[0] == 'C')
+            {
+                //Clear
+                operand_b = 0;
+                memory = 0;
+                lb_display.Content = memory;
+            }
+            else if (btn != null && btn.Content.ToString()[0] == '=')
+            {
+                //Calculate
+                if (selectedOperator != null)
+                {
+                    memory = selectedOperator(operand_b, memory);
+                    lb_display.Content = memory;
+                }
+            }
         }
     }
 }
---
+++ 02_AndroidPassword/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_AndroidPassword
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool selection;
+        string currentpw = "";
+        string secretpw = "684123";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            selection = true;
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (currentpw == secretpw)
+            {
+                MessageBox.Show("SIGNED IN");
+            }
+            
+
+            selection = false;
+            currentpw = "";
+            foreach (var item in grid.Children)
+            {
+                if (item is Ellipse)
+                {
+                    (item as Ellipse).Fill = Brushes.Blue;
+                }
+            }
+        }
+
+        private void Window_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (selection && e.Source!= null && e.Source is Ellipse)
+            {
+                Ellipse ellipse = e.Source as Ellipse;
+                if (!currentpw.Contains(ellipse.Tag as string))
+                {
+                    currentpw += ellipse.Tag as string;
+                    ellipse.Fill = Brushes.Red;
+                }
+            }
+        }
+
+    }
+}
